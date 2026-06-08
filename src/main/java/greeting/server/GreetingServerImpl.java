59c5aea04883378d0f3b71b7d9264fdb6f6eb88b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-06-08 10:40:04 -0300

    implement greeting many times impl

--- src/main/java/greeting/server/GreetingServerImpl.java
+++ src/main/java/greeting/server/GreetingServerImpl.java
@@ -5,10 +5,24 @@ import com.proto.greeting.GreetingResponse;
 import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
+import java.util.stream.IntStream;
+
 public class GreetingServerImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
     @Override
     public void greet(final GreetingRequest request, final StreamObserver<GreetingResponse> responseObserver) {
         responseObserver.onNext(GreetingResponse.newBuilder().setResult("Hello " + request.getFirstName()).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void greetManyTimes(final GreetingRequest request, final StreamObserver<GreetingResponse> responseObserver) {
+        GreetingResponse response = GreetingResponse.newBuilder().setResult("Hello " + request.getFirstName()).build();
+
+        IntStream.range(0, 10)
+                .forEach(number -> {
+                    responseObserver.onNext(response);
+                });
+
         responseObserver.onCompleted();
     }
 }
