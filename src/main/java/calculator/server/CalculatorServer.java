5a0cbaeb16a57fc2ca608782f8b44349de32c4aa
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-06-08 10:33:12 -0300

    add sum api

---
+++ src/main/java/calculator/server/CalculatorServer.java
@@ -0,0 +1,28 @@
+package calculator.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class CalculatorServer {
+    private static final int PORT = 50052;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(PORT)
+                .addService(new CalculatorServiceImpl())
+                .build();
+
+        server.start();
+        System.out.println("Server Started");
+        System.out.println("Listening on port: " + PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Server Stopped");
+        }));
+
+        server.awaitTermination();
+    }
+}
