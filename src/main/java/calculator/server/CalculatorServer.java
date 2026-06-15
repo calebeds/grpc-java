e1eccab9b761e1f9912bfe6b92f563e7cb9368b6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-06-15 09:16:36 -0300

    add reflection to CalculatorServer

--- src/main/java/calculator/server/CalculatorServer.java
+++ src/main/java/calculator/server/CalculatorServer.java
@@ -2,6 +2,7 @@ package calculator.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ public class CalculatorServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server = ServerBuilder.forPort(PORT)
                 .addService(new CalculatorServiceImpl())
+                .addService(ProtoReflectionService.newInstance())
                 .build();
 
         server.start();
